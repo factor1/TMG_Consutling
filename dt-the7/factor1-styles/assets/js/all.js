d100938aa4d5b370ed48355fb9dd5e6ffaa73eb5
Eric Stout <ericwstout@gmail.com>
2016-08-29 16:42:14 -0700

    Adds dom ready wrapper

--- dt-the7/factor1-styles/assets/js/all.js
+++ dt-the7/factor1-styles/assets/js/all.js
@@ -4,15 +4,17 @@ console.log('[Success! - Factor1 JS Initialized]');
 var siteURL = 'http://factor1.me/2016/tmg';
 
 // if we are on the archive page, load initial posts
-if( jQuery('body').hasClass('post-type-archive-tmg_resources') ){
-  console.log('Has correct body class for Rest API');
-  jQuery.ajax({
-    method: 'GET',
-    url: siteURL + '/wp-json/wp/v2/tmg_resources/',
-    data: { get_param: 'value'},
-    dataType: 'json',
-    success: function(posts){
-      console.log('[Loaded Posts Successfully]');
-    }
-  });
-}
+jQuery(document).ready(function(){
+  if( jQuery('body').hasClass('post-type-archive-tmg_resources') ){
+    console.log('Has correct body class for Rest API');
+    jQuery.ajax({
+      method: 'GET',
+      url: siteURL + '/wp-json/wp/v2/tmg_resources/',
+      data: { get_param: 'value'},
+      dataType: 'json',
+      success: function(posts){
+        console.log('[Loaded Posts Successfully]');
+      }
+    });
+  }
+});
