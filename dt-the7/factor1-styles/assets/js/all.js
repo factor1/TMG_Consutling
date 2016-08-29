6427cd94f50c10c5cb35ad575aca5bb02c9675e3
Eric Stout <ericwstout@gmail.com>
2016-08-29 16:39:33 -0700

    Adds console log for debugging

--- dt-the7/factor1-styles/assets/js/all.js
+++ dt-the7/factor1-styles/assets/js/all.js
@@ -5,6 +5,7 @@ var siteURL = 'http://factor1.me/2016/tmg';
 
 // if we are on the archive page, load initial posts
 if( jQuery('body').hasClass('post-type-archive-tmg_resources') ){
+  console.log('Has correct body class for Rest API');
   jQuery.ajax({
     method: 'GET',
     url: siteURL + '/wp-json/wp/v2/tmg_resources/',
