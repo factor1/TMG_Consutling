5be62d15ddd8287f95f03b9d933a4c56974a7515
Eric Stout <ericwstout@gmail.com>
2016-08-29 16:37:40 -0700

    Adds initial ajax pull for posts

--- dt-the7/factor1-styles/assets/js/factor1.js
+++ dt-the7/factor1-styles/assets/js/factor1.js
@@ -1,1 +1,17 @@
 console.log('[Success! - Factor1 JS Initialized]');
+
+// set crucial variables
+var siteURL = 'http://factor1.me/2016/tmg';
+
+// if we are on the archive page, load initial posts
+if( $('body').hasClass('post-type-archive-tmg_resources') ){
+  $.ajax({
+    method: 'GET',
+    url: siteURL + '/wp-json/wp/v2/tmg_resources/',
+    data: { get_param: 'value'},
+    dataType: 'json',
+    success: function(posts){
+      console.log('[Loaded Posts Successfully]');
+    }
+  });
+}
