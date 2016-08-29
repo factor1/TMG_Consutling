3f562dfd499efe75dab2f02f0d39b1362a4b6102
Eric Stout <ericwstout@gmail.com>
2016-08-29 14:38:02 -0700

    Updates to resource archive page

--- dt-the7/archive-tmg_resources.php
+++ dt-the7/archive-tmg_resources.php
@@ -1,6 +1,27 @@
 <?php
 get_header();
+?>
 
-echo '<h1>Archive Page</h1>';
+<section class="container">
+  <div class="row">
+    <div class="col-12">
+      <h1>
+        Resources
+      </h1>
+      <p>
+        RIMSolutions™, the research and innovation practice of TMG Consulting,
+        provides membership-based and complementary research into the relationship
+        between utilities and their customers and technologies that support stronger,
+        more satisfying connections. Additionally, we offer commissioned studies
+        for utilities and solution providers, along with whitepapers and presentations
+        for external audiences.
+      </p>
+      <p>
+        Contact us at 512.993.6331 to learn more or email <a href="mailto:research@tmgconsulting.com">researcg@tmgconsulting.com</a>.
+      </p>
+    </div>
+  </div>
+</section>
 
+<?php
 get_footer();
