0bbcba0f40a5b07b6cb2d7347cbafd54ebc328d4
Eric Stout <ericwstout@gmail.com>
2016-08-29 15:35:40 -0700

    Adds Rest API support for CPT

--- dt-the7/functions.php
+++ dt-the7/functions.php
@@ -31,3 +31,21 @@ function f1_styles() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'f1_styles' );
+
+
+/**
+  * Add REST API support to an already registered post type.
+  */
+  add_action( 'init', 'my_custom_post_type_rest_support', 25 );
+  function tmg_resources_rest_support() {
+  	global $wp_post_types;
+
+  	//be sure to set this to the name of your post type!
+  	$post_type_name = 'tmg_resources';
+  	if( isset( $wp_post_types[ $post_type_name ] ) ) {
+  		$wp_post_types[$post_type_name]->show_in_rest = true;
+  		$wp_post_types[$post_type_name]->rest_base = $post_type_name;
+  		$wp_post_types[$post_type_name]->rest_controller_class = 'WP_REST_Posts_Controller';
+  	}
+
+  }
