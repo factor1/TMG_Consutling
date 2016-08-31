7b5ab876656f0a91d4853fa0a1ec467d7bfa0ab5
Eric Stout <ericwstout@gmail.com>
2016-08-31 14:00:18 -0700

    Adds wordpress form function

--- dt-the7/single-tmg_resources.php
+++ dt-the7/single-tmg_resources.php
@@ -127,22 +127,7 @@ endif;?>
     </div>
     <div class="row">
       <div class="col-12 text-center">
-        <form name="loginform" id="loginform" action="<?php echo get_home_url();?>/wp-login.php" method="post">
-        	<p>
-        		<label for="user_login">Username or Email<br />
-        		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
-        	</p>
-        	<p>
-        		<label for="user_pass">Password<br />
-        		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
-        	</p>
-        		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
-        	<p class="submit">
-        		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-        		<input type="hidden" name="redirect_to" value="<?php echo get_home_url();?>/wp-admin/" />
-        		<input type="hidden" name="testcookie" value="1" />
-        	</p>
-        </form>
+        <?php wp_login_form(); ?>
       </div>
     </div>
   </div>
@@ -153,6 +138,10 @@ endif;?>
 jQuery('.f1login').click(function() {
   console.log('button clicked');
   jQuery('#f1-modal.modal').css('display','block');
+});
+
+jQuery('.f1-close-thik').click(function(){
+  jQuery('#f1-modal.modal').css('display','none');
 });
 </script>
 
