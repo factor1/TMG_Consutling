292cf7afeb88ab7b3473434685bd18c07359d6a4
Eric Stout <ericwstout@gmail.com>
2016-08-31 11:37:55 -0700

    Updates login form submission url

--- dt-the7/test-login.php
+++ dt-the7/test-login.php
@@ -1,7 +1,7 @@
 <?php
 /* Template Name: Test Login */
 get_header();?>
-<form name="loginform" id="loginform" action="http://tmgconsulting.com/wp-login.php" method="post">
+<form name="loginform" id="loginform" action="<?php echo get_home_url();?>/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -13,7 +13,7 @@ get_header();?>
 		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-		<input type="hidden" name="redirect_to" value="http://tmgconsulting.com/wp-admin/" />
+		<input type="hidden" name="redirect_to" value="<?php echo get_home_url();?>/wp-admin/" />
 		<input type="hidden" name="testcookie" value="1" />
 	</p>
 </form>
